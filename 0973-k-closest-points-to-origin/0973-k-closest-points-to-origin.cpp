4ec1253440f5f0b3c96a82323aace128d05cf500
Anshika Rathi <89533063+anshikarathi66@users.noreply.github.com>
2023-02-23 10:44:40 +0530

    Time: 243 ms (68.52%), Space: 71.5 MB (47.08%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        vector<pair<int,pair<int,int>>> v;
+        for(auto x:points){
+            //x2,y2=(0,0) also we're not interested in actual dist therefore we have not used sqrt to save complexity
+            int dist=(x[0]*x[0] + x[1]*x[1]);
+            v.push_back({dist, {x[0],x[1]}});
+        }
+        
+        sort(v.begin(),v.end());
+        vector<vector<int>> ans;
+        for(int i=0;i<k;i++){
+            ans.push_back({v[i].second.first, v[i].second.second});
+        }
+        
+        return ans;
+    }
+};
