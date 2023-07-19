12f94b37d9dc92054dd6b8f47d2b3333bf417d09
Anshika Rathi <89533063+anshikarathi66@users.noreply.github.com>
2023-07-20 02:09:41 +0530

    Time: 12 ms (17.02%), Space: 13.8 MB (41.50%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -28,11 +28,26 @@ public:
         return dp[n] = (min(solveMem(cost, n-1,dp) , solveMem(cost , n-2,dp)) + cost[n]);
     }
     
+    int solveTab(vector<int>& cost, int n){
+        vector<int> dp(n+1,-1);
+        
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        
+        for(int i = 2;i<n;i++){
+            dp[i] = min(dp[i-1] , dp[i-2]) + cost[i];
+        }
+        
+        return min(dp[n-1],dp[n-2]);
+    }
+    
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();  
         // return min(solve(cost, n-1) , solve(cost , n-2)); //for last (nth) step since it doesn't need any cost to be at last step
         
-        vector<int> dp(n+1 , -1);
-        return min(solveMem(cost,n-1,dp), solveMem(cost,n-2,dp));
+        // vector<int> dp(n+1 , -1);
+        // return min(solveMem(cost,n-1,dp), solveMem(cost,n-2,dp));
+        
+        return solveTab(cost, n);
     }
 };
