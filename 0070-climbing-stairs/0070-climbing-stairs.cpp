04dae2ee462b72f6b78c3201533b9f90e14357de
Anshika Rathi <89533063+anshikarathi66@users.noreply.github.com>
2023-07-19 22:00:50 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (36.51%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(int n, vector<int> &dp){
+    int solveMem(int n, vector<int> &dp){
         if(n<0)
             return 0;
         if(n==0)
@@ -10,23 +10,45 @@ public:
             return dp[n];
         }
         
-        dp[n] = solve(n-1, dp) + solve(n-2, dp);
+        dp[n] = solveMem(n-1, dp) + solveMem(n-2, dp);
         return dp[n];
     }
+    
+    int solve(int n){
+        //base case
+        if(n<0) //since no negative stairs exists
+            return 0;
+        if(n == 0)  //since no. of ways of going from 0th stair to 0th stair are one
+            return 1;
+        
+        //recursive call
+        return climbStairs(n-1) + climbStairs(n-2); //no of ways for climbing n-1th stair
+                                                    //+ 
+                                                    //no of ways for climbing n-2th stair
+        //since at last stair it can either come from n-1th stair or n-2th stair
+        
+    }
+    
+    int solvetab(int n){
+        vector<int> dp(n+1,0);
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i = 2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n];
+    }
+    
     int climbStairs(int n) {
-//         //base case
-//         if(n<0) //since no negative stairs exists
-//             return 0;
-//         if(n == 0)  //since no. of ways of going from 0th stair to 0th stair are one
-//             return 1;
         
-//         //recursive call
-//         return climbStairs(n-1) + climbStairs(n-2); //no of ways for climbing n-1th stair
-//                                                     //+ 
-//                                                     //no of ways for climbing n-2th stair
-//         //since at last stair it can either come from n-1th stair or n-2th stair
+        // return solve(n);
         
-        vector<int> dp(n+1,-1);
-        return solve(n,dp);
+        // vector<int> dp(n+1,-1);
+        // return solveMem(n,dp);
+        
+        return solvetab(n);
     }
 };
